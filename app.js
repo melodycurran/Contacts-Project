f9e7f13ce2e2ca8c9f8b4271f22960ae54e4c0e7
Melody <cur21014@byui.edu>
2023-09-21 17:09:41 -0600

    connected mongodb

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongo = require('./data/database.js');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require("./routes"));
 
-app.listen(port, () => {
-    console.log(`The Web Sever is listening at ${port}`);
+mongo.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log(`The database and Web Sever is listening at ${port}`);
+        })        
+    }
 })
+
 
