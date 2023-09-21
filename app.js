064e850c0d3fc9d164d71a1f11ee6bbc427fa393
Melody <cur21014@byui.edu>
2023-09-21 15:59:53 -0600

    created web server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/', require("./routes"));
+
 app.listen(port, () => {
     console.log(`The Web Sever is listening at ${port}`);
 })
