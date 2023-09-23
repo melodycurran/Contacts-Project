fad528b88e54ee056f584eb54f78789819063e4a
Melody <cur21014@byui.edu>
2023-09-22 23:46:04 -0600

    Tried to fix error

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-router.use('./docs', swaggerUI.serve);
-router.get('./docs', swaggerUI.setup(swaggerDocument));
+router.use('/docs', swaggerUI.serve);
+router.get('/docs', swaggerUI.setup(swaggerDocument));
 
 module.exports = router;
